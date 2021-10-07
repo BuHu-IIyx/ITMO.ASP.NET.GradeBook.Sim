b04390b42e7411ac51c0ad9baa045fc9d1a96f40
Alexander (BuHu-IIyx) <Buhu.iiyxs@gmail.com>
2021-10-07 12:01:30 +0300

    That's all (or not)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using ITMO.ASP.NET.GradeBook.Sim.Models;
+using System.Net;
 
 namespace ITMO.ASP.NET.GradeBook.Sim.Controllers
 {
@@ -24,6 +26,47 @@ namespace ITMO.ASP.NET.GradeBook.Sim.Controllers
         {
             ViewBag.MidGrade = CalculateMid.Midle(GBook, true);
             return View();
+        }
+        [HttpGet]
+        public ActionResult EditGrade(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GradeBookModel gradeBookModel = GBook.gradeBookModels.Find(id);
+            if (gradeBookModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gradeBookModel);
+        }
+        [HttpPost]
+        public ActionResult EditGrade(GradeBookModel gradeBook)
+        {
+            if (ModelState.IsValid)
+            {                
+                    GBook.Entry(gradeBook).State = EntityState.Modified;
+                    GBook.SaveChanges();
+                    return RedirectToAction("Index");
+            }    
+            return View(gradeBook);
+        }
+        [HttpGet]
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddStudent(GradeBookModel gradeBook)
+        {
+            if (ModelState.IsValid)
+            {
+                GBook.gradeBookModels.Add(gradeBook);
+                GBook.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gradeBook);
         }
     }
 }
--- Models/GradeBookModel.cs
+++ Models/GradeBookModel.cs
@@ -19,35 +19,36 @@ namespace ITMO.ASP.NET.GradeBook.Sim.Models
     {
         [Key]
         [DisplayName("ID Студента")]
+        [Required]
         public virtual int GradeBookID { get; set; }
         [DisplayName("Имя студента")]
         [Required(ErrorMessage = "ФИО студента обязательно для заполнения.")]
         public virtual string StudentName { get; set; }
         [DisplayName("Номер группы")]
-        [Required(ErrorMessage = "Номер группы обязательно для заполнения.")]
+        [Required(ErrorMessage = "Номер группы обязателен для заполнения.")]
         public virtual string Group { get; set; }
-        [Range(1, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]
+        [Range(0, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]
         [DisplayName("Технологии программирования")]
         public virtual int Grade1 { get; set; }
-        [Range(1, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
+        [Range(0, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
         [DisplayName("Введение в БД")]
         public virtual int Grade2 { get; set; }
-        [Range(1, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
+        [Range(0, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
         [DisplayName("C#")]
         public virtual int Grade3 { get; set; }
-        [Range(1, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
+        [Range(0, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
         [DisplayName("TSQL")]
         public virtual int Grade4 { get; set; }
-        [Range(1, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
+        [Range(0, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
         [DisplayName("Разработка Win-приложений")]
         public virtual int Grade5 { get; set; }
-        [Range(1, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
+        [Range(0, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
         [DisplayName("HTML and CSS")]
         public virtual int Grade6 { get; set; }
-        [Range(1, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
+        [Range(0, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
         [DisplayName("ADO.NET")]
         public virtual int Grade7 { get; set; }
-        [Range(1, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
+        [Range(0, 5, ErrorMessage = "Допустимы значения от {1} до {2}.")]        
         [DisplayName("ASP.NET")]
         public virtual int Grade8 { get; set; }
     }    
